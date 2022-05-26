502c0cef33d0b2fbbc1b9eb20f3186f48ce22cb4
Rathna Natarajan <rnatarajan@axway.com>
2022-05-26 07:25:26 -0700

    Code cleanup

--- src/main/java/com/axway/ExternalInstanceDomainCert.java
+++ src/main/java/com/axway/ExternalInstanceDomainCert.java
@@ -34,17 +34,11 @@ public class ExternalInstanceDomainCert {
 
 
     public final static String LINE_SEPARATOR = System.getProperty("line.separator");
-    private Base64.Encoder encoder;
+    private final Base64.Encoder encoder;
 
     public ExternalInstanceDomainCert() {
         encoder = Base64.getMimeEncoder(64, LINE_SEPARATOR.getBytes());
     }
-
-
-    // File certsXml = new File(Config.getVDir("VINSTDIR"), "conf");
-    // File certsXml = new File("src/");
-    // certsXml = new File(certsXml, "certs.xml");
-
 
     public void updateMgmtFile(File mgmtFile, String CAAlias) throws ParserConfigurationException, IOException, SAXException, TransformerException {
         if (mgmtFile.exists()) {
@@ -73,9 +67,6 @@ public class ExternalInstanceDomainCert {
         FileWriter writer = new FileWriter(mgmtFile);
         StreamResult result = new StreamResult(writer);
         transformer.transform(source, result);
-//        byte[] data = Files.readAllBytes(Paths.get(mgmtFile.getAbsolutePath()));
-//        System.out.println(new String(data));
-
     }
 
     public String certsFile(PKCS12 pkcs12, File certsXml) throws IOException, CertificateException {
@@ -109,16 +100,9 @@ public class ExternalInstanceDomainCert {
             endCertificateElement(stringBuilder);
         }
         stringBuilder.append("</ConfigurationFragment>");
-        FileOutputStream fileOutputStream = null;
 
-        try {
-            fileOutputStream = new FileOutputStream(certsXml);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(certsXml)) {
             fileOutputStream.write(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
-            fileOutputStream.close();
-        } finally {
-            if (fileOutputStream != null) {
-                fileOutputStream.close();
-            }
         }
         return CAAlias;
     }
@@ -142,7 +126,7 @@ public class ExternalInstanceDomainCert {
 
 
     public static byte[] signData(byte[] data, PrivateKey key) {
-        Signature signer = null;
+        Signature signer;
         try {
             signer = Signature.getInstance("SHA256withRSA");
             signer.initSign(key);
@@ -155,7 +139,7 @@ public class ExternalInstanceDomainCert {
     }
 
     public static boolean verifySignature(byte[] data, PublicKey key, byte[] sig) {
-        Signature signer = null;
+        Signature signer;
         try {
             signer = Signature.getInstance("SHA256withRSA");
             signer.initVerify(key);
--- src/main/java/com/axway/Util.java
+++ src/main/java/com/axway/Util.java
@@ -12,20 +12,13 @@ public final  class  Util {
 
         Map<String, Map<String, String>> values = new HashMap<>();
         if (envMap != null && !envMap.isEmpty()) {
-            Iterator<String> keyIterator = envMap.keySet().iterator();
-            while (keyIterator.hasNext()) {
-                String key = keyIterator.next();
+            for (String key : envMap.keySet()) {
                 String[] delimitedKeys = key.split("_");
-                String filterName;
                 if (delimitedKeys.length == 3) {
-                    filterName = delimitedKeys[1];
+                    String filterName = delimitedKeys[1];
                     String attribute = delimitedKeys[2];
                     String value = envMap.get(key);
-                    Map<String, String> attributes = values.get(filterName);
-                    if (attributes == null) {
-                        attributes = new HashMap<>();
-                        values.put(filterName, attributes);
-                    }
+                    Map<String, String> attributes = values.computeIfAbsent(filterName, k -> new HashMap<>());
                     attributes.put(attribute, value);
                 }
             }
@@ -41,7 +34,6 @@ public final  class  Util {
     }
 
     public static String getAliasName(X509Certificate certificate){
-
 
         String alias = certificate.getSubjectDN().getName();
         if (alias.equals("")) {
